5ae6292dadf662f8ab9563958e95fb8e46503d69
Alex Racinski <aracinski2@gmail.com>
2020-12-17 20:36:39 -0600

    run migration to change recipes instructions to text type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_014637) do
+ActiveRecord::Schema.define(version: 2020_12_18_022649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_12_18_014637) do
     t.string "alcoholic"
     t.string "ingredients"
     t.string "measurements"
-    t.string "instructions"
+    t.text "instructions"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# ingredient = Ingredient.create(name: "orange bitters", description: "this is an orange bitter and stuff", category: "bitters", alcohol: "no")
-# ingredient = Ingredient.create(name: "irish whiskey", description: "this is an irish whiskey and stuff", category: "whiskey", alcohol: "yes", abv: 40)
-# ingredient = Ingredient.create(name: "absolut vodka", description: "this is a vodka and stuff", category: "vodka", alcohol: "yes", abv: 40)
-# ingredient = Ingredient.create(name: "lemon absolut", description: "this is a vodka and stuff", category: "vodka", alcohol: "yes", abv: 35)
+ingredient = Ingredient.create(name: "orange bitters", description: "this is an orange bitter and stuff", category: "bitters", alcohol: "no")
+ingredient = Ingredient.create(name: "irish whiskey", description: "this is an irish whiskey and stuff", category: "whiskey", alcohol: "yes", abv: 40)
+ingredient = Ingredient.create(name: "absolut vodka", description: "this is a vodka and stuff", category: "vodka", alcohol: "yes", abv: 40)
+ingredient = Ingredient.create(name: "lemon absolut", description: "this is a vodka and stuff", category: "vodka", alcohol: "yes", abv: 35)
 
-# ingredient = Ingredient.create(name: "orange juice", description: "this is a juice and stuff", category: "juice", alcohol: "no")
-# ingredient = Ingredient.create(name: "watermelon absolut", description: "this is a vodka and stuff", category: "vodka", alcohol: "yes", abv: 30)
-# ingredient = Ingredient.create(name: "lemon juice", description: "this is a juice and stuff", category: "juice", alcohol: "no")
+ingredient = Ingredient.create(name: "orange juice", description: "this is a juice and stuff", category: "juice", alcohol: "no")
+ingredient = Ingredient.create(name: "watermelon absolut", description: "this is a vodka and stuff", category: "vodka", alcohol: "yes", abv: 30)
+ingredient = Ingredient.create(name: "lemon juice", description: "this is a juice and stuff", category: "juice", alcohol: "no")
 
-# recipe = Recipe.create(name: "screwdriver", alcoholic: "yes", ingredients: ["absolut vodka", "orange juice"], measurements: ["fsd", "sdfadf"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
+recipe = Recipe.create(name: "screwdriver", alcoholic: "yes", ingredients: ["absolut vodka", "orange juice"], measurements: ["fsd", "sdfadf"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
+recipe = Recipe.create(name: "whiskey oj", alcoholic: "yes", ingredients: ["whiskey", "orange juice"], measurements: ["fsd3242", "sadfas23"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkd245q3453434fjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
+recipe = Recipe.create(name: "long island", alcoholic: "yes", ingredients: ["watermelon absolut", "lemon juice"], measurements: ["fs234234d", "sdfa2342df"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkreq3h4434u8qpo8o8uypdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
+recipe = Recipe.create(name: "old fashioned", alcoholic: "yes", ingredients: ["irish whiskey", "orange bitters", "lemon juice"], measurements: ["asdfacs", "geweerg", "adhflkuwh8"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkreq3h4434asdfjasdkfjaju8qpo8o8uypdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
+
+# favorite = Favorite.create(user_id: 1, recipe_id: 1)
+# favorite = Favorite.create(user_id: 1, recipe_id: 2)
+# favorite = Favorite.create(user_id: 2, recipe_id: 2)
 
 
