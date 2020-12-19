51a228da73777963e7b02e7ccb584ab5e4d9fcb5
Alex Racinski <aracinski2@gmail.com>
2020-12-19 15:59:31 -0600

    add associations to recipe model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+# user = User.create(first_name: "alex", last_name: "r", email: "alex@gmail.com")
+# user = User.create(first_name: "jim", last_name: "bob", email: "jimbob@gmail.com")
+
+
 # ingredient = Ingredient.create(name: "orange bitters", description: "this is an orange bitter and stuff", category: "bitters", alcohol: "no")
 # ingredient = Ingredient.create(name: "irish whiskey", description: "this is an irish whiskey and stuff", category: "whiskey", alcohol: "yes", abv: 40)
 # ingredient = Ingredient.create(name: "absolut vodka", description: "this is a vodka and stuff", category: "vodka", alcohol: "yes", abv: 40)
@@ -16,15 +20,13 @@
 # ingredient = Ingredient.create(name: "watermelon absolut", description: "this is a vodka and stuff", category: "vodka", alcohol: "yes", abv: 30)
 # ingredient = Ingredient.create(name: "lemon juice", description: "this is a juice and stuff", category: "juice", alcohol: "no")
 
-# recipe = Recipe.create(name: "screwdriver", alcoholic: "yes", ingredients: ["absolut vodka", "orange juice"], measurements: ["fsd", "sdfadf"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
-# recipe = Recipe.create(name: "whiskey oj", alcoholic: "yes", ingredients: ["whiskey", "orange juice"], measurements: ["fsd3242", "sadfas23"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkd245q3453434fjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
-# recipe = Recipe.create(name: "long island", alcoholic: "yes", ingredients: ["watermelon absolut", "lemon juice"], measurements: ["fs234234d", "sdfa2342df"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkreq3h4434u8qpo8o8uypdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
-# recipe = Recipe.create(name: "old fashioned", alcoholic: "yes", ingredients: ["irish whiskey", "orange bitters", "lemon juice"], measurements: ["asdfacs", "geweerg", "adhflkuwh8"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkreq3h4434asdfjasdkfjaju8qpo8o8uypdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
+# recipe = Recipe.create(name: "screwdriver", alcoholic: "yes", specific_ingredients: ["absolut vodka", "orange juice"], measurements: ["fsd", "sdfadf"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
+# recipe = Recipe.create(name: "whiskey oj", alcoholic: "yes", specific_ingredients: ["whiskey", "orange juice"], measurements: ["fsd3242", "sadfas23"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkd245q3453434fjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
+# recipe = Recipe.create(name: "long island", alcoholic: "yes", specific_ingredients: ["watermelon absolut", "lemon juice"], measurements: ["fs234234d", "sdfa2342df"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkreq3h4434u8qpo8o8uypdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
+# recipe = Recipe.create(name: "old fashioned", alcoholic: "yes", specific_ingredients: ["irish whiskey", "orange bitters", "lemon juice"], measurements: ["asdfacs", "geweerg", "adhflkuwh8"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkreq3h4434asdfjasdkfjaju8qpo8o8uypdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
 
 # favorite = Favorite.create(user_id: 1, recipe_id: 1)
 # favorite = Favorite.create(user_id: 1, recipe_id: 2)
 # favorite = Favorite.create(user_id: 2, recipe_id: 2)
 
-recipe = Recipe.create(name: "screwdriver", alcoholic: "yes", test: ["absolut vodka", "orange juice"], measurements: ["fsd", "sdfadf"], instructions: "asfakljdfklasjdfkjaksdfjajsfja;kfj;jksjlkfjdsfa;lksdjflkajsdflkjasdlkfjaslkdfjlaksjdflkasjkljafajksdhf;ajfdkjaskdfj;lakjsdf;jsadkfjslkjfd;lkj")
-
-
+rails 
