42bfe53d9e4cc6c44473d1c4797c8147f0750675
Alex Racinski <aracinski2@gmail.com>
2021-01-01 16:07:31 -0600

    add recipes index route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,17 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/recipes" => "recipes#index"
+    get "/recipes/:id" => "recipes#show"
+
+    get "/ingredients" => "ingredients#index"
+
+    get "/favorites" => "favorites#index"
+    post "/favorites" => "favorites#create"
+    delete "/favorites/:id" => "favorites#destroy"
+
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
+  end
 end
