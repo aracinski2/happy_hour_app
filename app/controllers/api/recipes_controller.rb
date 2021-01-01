7b0259f0e91c3bf43af2e85f6425bba16cb63a9f
Alex Racinski <aracinski2@gmail.com>
2021-01-01 16:19:36 -0600

    update recipes index view, add recipes show action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -4,4 +4,9 @@ class Api::RecipesController < ApplicationController
 
     render 'index.json.jb'
   end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
 end
