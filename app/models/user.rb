d267f141ecf9dec81ab30564d2eccf24e7c90c78
Alex Racinski <aracinski2@gmail.com>
2021-01-01 17:18:23 -0600

    remove first/last name, add name to users model, add validation to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+
   has_many :favorites
   has_many :recipes, through: :favorites
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_01_225608) do
+ActiveRecord::Schema.define(version: 2021_01_01_231709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,12 +49,11 @@ ActiveRecord::Schema.define(version: 2021_01_01_225608) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "name"
   end
 
 end
