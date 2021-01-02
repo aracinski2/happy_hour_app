7df43120362c8d2963e8828cc4214e93b5d04c9f
Alex Racinski <aracinski2@gmail.com>
2021-01-01 19:28:06 -0600

    make recipes index dynamic based on search type, reverse ingredients index to ingreadients.all

--- app/controllers/api/ingredients_controller.rb
+++ app/controllers/api/ingredients_controller.rb
@@ -1,6 +1,9 @@
 class Api::IngredientsController < ApplicationController
   def index
     @ingredients = Ingredient.all
+    # ingredient = Ingredient.find_by(name: params[:name])
+    
+    # @ingredients = ingredient.recipes
     render 'index.json.jb'
   end
 end
--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,6 +1,18 @@
 class Api::RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    # @recipes = Recipe.all
+    search_term = params[:search]
+
+    # category = Ingredient.find_by(params[:search].to_sym: params[:category])
+    # @recipes = category.recipes
+
+    if search_term == "category"
+      category = Ingredient.find_by(category: params[:category])
+      @recipes = category.recipes
+    elsif search_term == "ingredient"
+      name = Ingredient.find_by(name: params[:name])
+      @recipes = name.recipes
+    end
 
     render 'index.json.jb'
   end
