4373b4a1d7b35610bac136cec56d7c858cac39c2
Alex Racinski <aracinski2@gmail.com>
2021-01-04 21:40:35 -0600

    hardcode recipes index to filter recipes by category

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,18 +1,25 @@
 class Api::RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
-    # search_term = params[:search]
+    # @recipes = Recipe.all
+    search_term = "category"
 
     # category = Ingredient.find_by(params[:search].to_sym: params[:category])
     # @recipes = category.recipes
 
-    # if search_term == "category"
-    #   category = Ingredient.find_by(category: params[:category])
-    #   @recipes = category.recipes
-    # elsif search_term == "ingredient"
-    #   name = Ingredient.find_by(name: params[:name])
-    #   @recipes = name.recipes
-    # end
+    if search_term == "category"
+      category = Ingredient.where(category: "Whisky")
+      category_recipes = []
+      category.each do |ingredient|
+        ingredient = ingredient.recipes
+        ingredient.each do |ingredient_recipes|
+          category_recipes << ingredient_recipes
+        end
+      end
+      @recipes = category_recipes
+    elsif search_term == "ingredient"
+      name = Ingredient.where(name: params[:name])
+      @recipes = name.recipes
+    end
 
     render 'index.json.jb'
   end
