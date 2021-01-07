ba312e3ca910d297e230f2c52ba6c26570a188a5
Alex Racinski <aracinski2@gmail.com>
2021-01-06 19:48:19 -0600

    change favorites create to not load index, add recipe to index view

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -3,15 +3,17 @@ class Api::FavoritesController < ApplicationController
   before_action :authenticate_user 
 
   def index
-    @favorites  = Favorite.all
+    @favorites  = Favorite.where(user_id: current_user.id)
     render 'index.json.jb'
   end
 
   def create
-    @favorite = Favorite.create(
+    @favorite = Favorite.new(
       user_id: current_user.id,
       recipe_id: params[:recipe_id]
     )
+    @favorite.save
+    p "favoriteeeeeeeee"
     # render 'index.json.jb'
   end
 
